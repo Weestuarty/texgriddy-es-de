2f4faebec8a8873b6960e3541299f6feb2a8fc54
Weestuarty <93288988+Weestuarty@users.noreply.github.com>
2023-04-30 15:55:05 +0100

    fix

--- sysmetaadd.bat
+++ sysmetaadd.bat
@@ -3,4 +3,4 @@ git subtree add --prefix=system/metadata --squash system-metadata master
 git remote add system-logos https://gitlab.com/es-de/themes/system-logos.git
 git subtree add --prefix=system/logos --squash system-logos master
 git remote add syscustom https://github.com/Weestuarty/syslogo.git
-git subtree add --prefix=system/custom --squash syscustom main
+git subtree merge --prefix=system --squash syscustom main
