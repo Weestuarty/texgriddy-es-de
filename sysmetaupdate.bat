45038c0b245007cb47b1e8658c8e81dd72a681e7
Weestuarty <93288988+Weestuarty@users.noreply.github.com>
2023-04-30 15:37:35 +0100

    Update sysmetaupdate.bat

--- sysmetaupdate.bat
+++ sysmetaupdate.bat
@@ -1,3 +1,3 @@
 git subtree pull --prefix=system/metadata --squash system-metadata master
 git subtree pull --prefix=system/logos --squash syslogo main
-git subtree pull --prefix=system/custom --squash syscustom main
+git subtree pull --prefix=system --squash syscustom main
